564ae7a163744d5d088695f2d42bbf0621b1e699
Renato Castro <rspindola@live.com>
2022-12-23 15:50:32 -0300

    feat: add formik and form modal

---
+++ src/components/NewGuestModal.tsx
@@ -0,0 +1,229 @@
+import { Fragment, useRef } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+
+import { useFormik } from 'formik'
+import * as yup from 'yup'
+
+type ModalProps = {
+  toggle: boolean
+  title: string
+  action: () => void
+}
+
+export default function NewGuestModal({ toggle, action }: ModalProps) {
+  const cancelButtonRef = useRef(null)
+
+  const formik = useFormik({
+    initialValues: {
+      origin: '',
+      treatment: '',
+      name: '',
+      escorts: 0,
+      men: 0,
+      women: 0,
+      children: 0,
+    },
+    onSubmit: (values, actions) => {
+      console.log('entrou')
+      console.log(values)
+      actions.setSubmitting(false)
+      actions.resetForm()
+    },
+    validationSchema: yup.object({
+      origin: yup.string().trim().required('Name is required'),
+      treatment: yup.string().trim().required('Name is required'),
+      name: yup.string().trim().required('Name is required'),
+      escorts: yup.number().required('Name is required'),
+      men: yup.number().required('Name is required'),
+      women: yup.number().required('Name is required'),
+      children: yup.number().required('Name is required'),
+    }),
+  })
+
+  return (
+    <Transition.Root show={toggle} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={action}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <form onSubmit={() => formik.handleSubmit()}>
+                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <Dialog.Title
+                          as="h3"
+                          className="text-lg font-medium leading-6 text-gray-900"
+                        >
+                          Incluir convidado
+                        </Dialog.Title>
+                        <div className="mt-4">
+                          <div className="flex flex-col max-w-md bg-white rounded-lg shadow dark:bg-gray-800">
+                            <div className="flex-col mb-2">
+                              <div className="relative">
+                                <select
+                                  className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                                  name="origin"
+                                  value={formik.values.origin}
+                                  onChange={formik.handleChange}
+                                  onBlur={formik.handleBlur}
+                                >
+                                  <option value="Familia do noivo">
+                                    Familia do noivo
+                                  </option>
+                                  <option value="Familia da noiva">
+                                    Familia da noiva
+                                  </option>
+                                  <option value="Amigos do noivo<">
+                                    Amigos do noivo
+                                  </option>
+                                  <option value="Amigos da noiva">
+                                    Amigos da noiva
+                                  </option>
+                                  <option value="Amigos dos pais do noivo">
+                                    Amigos dos pais do noivo
+                                  </option>
+                                  <option value="Amigos dos pais da noiva">
+                                    Amigos dos pais da noiva
+                                  </option>
+                                  <option value="Outros">Outros</option>
+                                </select>
+                              </div>
+                            </div>
+                            <div className="flex flex-col mb-2">
+                              <div className="relative">
+                                <div className="relative mt-1 rounded-md shadow-sm">
+                                  <div className="absolute inset-y-0 left-0 flex items-center">
+                                    <label
+                                      htmlFor="treatment"
+                                      className="sr-only"
+                                    >
+                                      Currency
+                                    </label>
+                                    <select
+                                      id="treatment"
+                                      name="treatment"
+                                      className="h-full pr-4 py-2 pl-2 text-gray-500 bg-transparent border-t border-b border-r border-transparent border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-r-md"
+                                      value={formik.values.treatment}
+                                      onChange={formik.handleChange}
+                                      onBlur={formik.handleBlur}
+                                    >
+                                      <option>Sr.</option>
+                                      <option>Sra.</option>
+                                      <option>Sr e Familia</option>
+                                      <option>Sra e Familia</option>
+                                    </select>
+                                  </div>
+                                  <input
+                                    type="text"
+                                    name="name"
+                                    id="name"
+                                    className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full pl-28 py-2 pr-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                                    placeholder="Nome Completo"
+                                    value={formik.values.name}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                  />
+                                </div>
+                              </div>
+                            </div>
+                            <div className="flex gap-4 mb-2">
+                              <div className="relative">
+                                <input
+                                  type="number"
+                                  id="create-account-first-name"
+                                  className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 pl-2 pr-1 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-sm focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                                  name="escorts"
+                                  placeholder="Acompanhantes"
+                                  value={formik.values.escorts}
+                                  onChange={formik.handleChange}
+                                  onBlur={formik.handleBlur}
+                                />
+                              </div>
+                              <div className="relative">
+                                <input
+                                  type="number"
+                                  id="create-account-last-name"
+                                  className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 pl-2 pr-1 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-sm focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                                  name="men"
+                                  placeholder="Homens"
+                                  value={formik.values.men}
+                                  onChange={formik.handleChange}
+                                  onBlur={formik.handleBlur}
+                                />
+                              </div>
+                              <div className="relative">
+                                <input
+                                  type="number"
+                                  id="create-account-last-name"
+                                  className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 pl-2 pr-1 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-sm focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                                  name="women"
+                                  placeholder="Mulheres"
+                                  value={formik.values.women}
+                                  onChange={formik.handleChange}
+                                  onBlur={formik.handleBlur}
+                                />
+                              </div>
+                              <div className="relative">
+                                <input
+                                  type="number"
+                                  id="create-account-last-name"
+                                  className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 pl-2 pr-1 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-sm focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                                  name="children"
+                                  placeholder="Crianças"
+                                  value={formik.values.children}
+                                  onChange={formik.handleChange}
+                                  onBlur={formik.handleBlur}
+                                />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button
+                      type="submit"
+                      className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                    >
+                      Salvar
+                    </button>
+                    <button
+                      type="button"
+                      className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                      onClick={action}
+                      ref={cancelButtonRef}
+                    >
+                      Cancelar
+                    </button>
+                  </div>
+                </form>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  )
+}
--- src/pages/guest.tsx
+++ src/pages/guest.tsx
@@ -5,6 +5,8 @@ import { GetServerSideProps } from 'next'
 import axios from '../lib/axios'
 
 import { toast } from 'react-toastify'
+import { useState } from 'react'
+import NewGuestModal from '../components/NewGuestModal'
 
 interface Data {
   id: number
@@ -42,6 +44,8 @@ export const getServerSideProps: GetServerSideProps<{
 export default function Page({
   data,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [open, setOpen] = useState(false)
+
   const handleInvite = async (id: number, event: React.ChangeEvent) => {
     const option = event.target as HTMLInputElement
     const isChecked = option.checked
@@ -125,10 +129,35 @@ export default function Page({
       <Head>
         <title>Wedding ADM - Lista de Convidados</title>
       </Head>
+
+      <NewGuestModal
+        title="Mensagem"
+        toggle={open}
+        action={() => setOpen(false)}
+      />
+
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div className="p-6 bg-white border-b border-gray-200">
+              <div className="flex justify-end w-full mb-4">
+                <button
+                  onClick={() => setOpen(true)}
+                  className="flex items-center px-6 py-2  transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none"
+                >
+                  <svg
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    viewBox="0 0 2304 1792"
+                    className="mr-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M1728 448l-384 704h768zm-1280 0l-384 704h768zm821-192q-14 40-45.5 71.5t-71.5 45.5v1291h608q14 0 23 9t9 23v64q0 14-9 23t-23 9h-1344q-14 0-23-9t-9-23v-64q0-14 9-23t23-9h608v-1291q-40-14-71.5-45.5t-45.5-71.5h-491q-14 0-23-9t-9-23v-64q0-14 9-23t23-9h491q21-57 70-92.5t111-35.5 111 35.5 70 92.5h491q14 0 23 9t9 23v64q0 14-9 23t-23 9h-491zm-181 16q33 0 56.5-23.5t23.5-56.5-23.5-56.5-56.5-23.5-56.5 23.5-23.5 56.5 23.5 56.5 56.5 23.5zm1088 880q0 73-46.5 131t-117.5 91-144.5 49.5-139.5 16.5-139.5-16.5-144.5-49.5-117.5-91-46.5-131q0-11 35-81t92-174.5 107-195.5 102-184 56-100q18-33 56-33t56 33q4 7 56 100t102 184 107 195.5 92 174.5 35 81zm-1280 0q0 73-46.5 131t-117.5 91-144.5 49.5-139.5 16.5-139.5-16.5-144.5-49.5-117.5-91-46.5-131q0-11 35-81t92-174.5 107-195.5 102-184 56-100q18-33 56-33t56 33q4 7 56 100t102 184 107 195.5 92 174.5 35 81z"></path>
+                  </svg>
+                  Adicionar
+                </button>
+              </div>
               <div className="overflow-x-auto custom-scroll">
                 <table className="table table-compact w-full table-auto">
                   <thead className="border-b">
